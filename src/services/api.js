8f8d6c657c3b0ccdff5b29ee8bfa237bce53cbbf
Jeferson de Oliveira Batista <jeferson.de.oliveira.batista@gmail.com>
2022-03-27 17:20:00 -0300

    fix: export API services both ways

--- src/services/api.js
+++ src/services/api.js
@@ -8,21 +8,21 @@ function createAuth(token) {
   return { headers: { Authorization: `Bearer ${token}` } };
 }
 
-const signUp = async (newUser) => instance.post("/users", newUser);
+export const signUp = async (newUser) => instance.post("/users", newUser);
 
-const signIn = async (userData) => instance.post("/sessions", userData);
+export const signIn = async (userData) => instance.post("/sessions", userData);
 
-const logout = async (token) => instance.delete("/sessions", createAuth(token));
+export const logout = async (token) => instance.delete("/sessions", createAuth(token));
 
-const publish = async (post, token) =>
+export const publish = async (post, token) =>
   instance.post("/posts", post, createAuth(token));
 
-const getPosts = async (token) => instance.get(`/posts`, createAuth(token));
+export const getPosts = async (token) => instance.get(`/posts`, createAuth(token));
 
-const getPostsId = async (id, token) =>
+export const getPostsId = async (id, token) =>
   instance.get(`/posts/${id}`, createAuth(token));
 
-const getUserByName = async (name) => instance.get(`/users/name`, name);
+export const getUserByName = async (name) => instance.get(`/users/name`, name);
 
 const api = {
   signUp,
