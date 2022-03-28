d12e9b26d00af5c1a90b655b2930c84a306fdf1f
ramon oliveira <ramon30012000@gmail.com>
2022-03-28 12:47:02 -0300

    feat: add edit post button layout with width post content bug

--- src/pages/Posts/Post.js
+++ src/pages/Posts/Post.js
@@ -1,14 +1,26 @@
+import { FaPencilAlt } from "react-icons/fa";
 import PostStyle from "../../components/postsComponents/postStyled";
 import Snippet from "../../components/postsComponents/snippet";
 import Curtidas from "../../components/curtidas";
 import { AiOutlineHeart as CurtidaIcon } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import useAuth from "../../hooks/useAuth";
 
 export default function Post({ list }) {
+	const { auth } = useAuth();
+
 	return list.map((p) => {
 		return (
 			<PostStyle key={p.id}>
+				{ // A user can only delete your own posts
+				p.name === auth.userName && 
+				(<FaPencilAlt
+					className="edit-icon"
+					size={20}
+					style={{fill: 'white'}}
+					onClick={() => console.log("editar!")}
+				/>)}
 				<section>
 					<img src={p.pictureUrl} alt="erro" />
 					<Curtidas>
